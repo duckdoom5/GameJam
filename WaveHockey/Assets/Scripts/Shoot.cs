46773f6f0610f3e1301f8a1ae15e3a88dbcd58ce
JChan92 <jeanchan1992@gmail.com>
2017-01-21 00:31:55 +0100

    Add Shooting Mechanic

---
+++ WaveHockey/Assets/Scripts/Shoot.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour {
+
+    public GameObject ball;
+    public GameObject player;
+    public int force;
+    public float cooldown;
+    private float counter;
+    public KeyCode keycode;
+
+	// Use this for initialization
+	void Start () {
+        counter = 0.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        counter -= Time.deltaTime;
+
+        if (Input.GetKeyDown(keycode) && counter <= 0.0f)
+        {
+            Debug.Log("Pressed");
+            Shooting();
+            counter = cooldown;
+        }
+	}
+
+    public void Shooting()
+    {
+        Vector3 dir = ball.transform.position - player.transform.position;
+        dir.Normalize();
+
+        ball.GetComponent<Rigidbody>().AddForce(dir*force);
+    }
+
+    
+}
