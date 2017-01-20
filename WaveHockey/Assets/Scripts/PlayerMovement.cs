a87af1e930352f34d7edf7b25c0c1964f4951908
umbacben <b.umbach@student.fontys.nl>
2017-01-21 00:08:57 +0100

    this has basic multiplayer tests

---
+++ WaveHockey/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerMovement : NetworkBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+        if (!isLocalPlayer)
+            return;
+        float x = Input.GetAxis("Horizontal")*.1f;
+        float z = Input.GetAxis("Vertical") * .1f;
+        transform.Translate(x, 0, z);
+	}
+}
