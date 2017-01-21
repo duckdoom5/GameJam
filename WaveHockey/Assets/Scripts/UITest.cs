acd2e4d6269731f5798e99e1164675596833934b
JChan92 <jeanchan1992@gmail.com>
2017-01-21 14:43:18 +0100

    UI(Beta1)

---
+++ WaveHockey/Assets/Scripts/UITest.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UITest : MonoBehaviour
+{
+    public GameObject _cooldownBar;
+    public GameObject _player;
+    private RectTransform trans;
+
+    private void Start()
+    {
+        trans = _cooldownBar.GetComponentInChildren<RectTransform>();
+    }
+
+    void Update()
+    {
+        trans.offsetMin = new Vector2(trans.offsetMin.x, 0);
+    }
+}
