175d71261cf699057a9ea6486600eb023747467d
umbacben <b.umbach@student.fontys.nl>
2017-01-21 02:00:55 +0100

    put stuff together

--- WaveHockey/Assets/Scripts/Player.cs
+++ WaveHockey/Assets/Scripts/Player.cs
@@ -7,19 +7,17 @@ public class Player : MonoBehaviour {
     [SerializeField] private KeyCode right = KeyCode.D;
     [SerializeField] private Vector2 elipseOrigin;
     [SerializeField] private Vector2 elipse;
-    [SerializeField] private float movementSpeed = 10.0f;
+    [SerializeField] private float movementSpeed = 1.0f;
 
-    [SerializeField] private Rigidbody rigidBody;
-    [SerializeField] private Collider collider;
-    private Player[] players;
+    [SerializeField] private new Rigidbody rigidbody;
+    [SerializeField] private new Collider collider;
 
-    [SerializeField] private float alpha, otherAngle;
+    [SerializeField] private float alpha;
     
     void Start()
     {
-        players = FindObjectsOfType<Player>();
         collider = GetComponent<Collider>();
-        rigidBody = GetComponent<Rigidbody>();
+        rigidbody = GetComponent<Rigidbody>();
     }
 
     float normalizeAngle(float angle)
@@ -29,7 +27,8 @@ public class Player : MonoBehaviour {
 
     // Update is called once per frame
     void Update () {
-        Vector3 oldPos = transform.position;
+        Vector3 pos = rigidbody.position;
+        alpha = Mathf.Atan2((pos.z - elipseOrigin.y) / elipse.y, (pos.x - elipseOrigin.x) / elipse.x);// *180.0f/Mathf.PI;
 
         // Update the player input
         if (Input.GetKey(left))
@@ -39,34 +38,10 @@ public class Player : MonoBehaviour {
             alpha += movementSpeed * Time.deltaTime;
 
         // Update the position based on an elipse
-        Vector3 pos = new Vector3();
         pos.x = elipseOrigin.x + (elipse.x * Mathf.Cos(alpha));
+        pos.y = 0.0f;
         pos.z = elipseOrigin.y + (elipse.y * Mathf.Sin(alpha));
 
-        alpha = normalizeAngle(alpha);
-
-        //float ratio = Mathf.Min(elipse.x, elipse.y) / Mathf.Max(elipse.x, elipse.y);
-        otherAngle = Mathf.Atan2(elipse.y / (pos.z - elipseOrigin.y), elipse.x / (pos.x - elipseOrigin.x));// *180.0f/Mathf.PI;
-        
-        transform.position = pos;
-
-        // Test if the player collided with another player
-        /*foreach(Player player in players) {
-            if(player == this)
-                continue;
-            
-            if (collider.bounds.Intersects(player.collider.bounds)) {
-                // If it did, revert to the old values and break
-                transform.position = oldPos;
-                alpha = oldAlpha;
-                break;
-            }
-        }*/
-        
+        rigidbody.position = pos;
 	}
-
-    /*private void OnCollisionEnter(Collision collision)
-    {
-        alpha = oldAlpha;
-    }*/
 }
