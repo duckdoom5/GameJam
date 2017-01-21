16ad24fbee63f70daee9b18fb6960e1c28a11ed4
JChan92 <jeanchan1992@gmail.com>
2017-01-21 11:14:33 +0100

    Added random spawning(beta1)

---
+++ WaveHockey/Assets/Scripts/Spawn.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour {
+    
+    public float _spawnrate;
+    private float _cooldown;
+
+	// Use this for initialization
+	void Start () {
+        _cooldown = _spawnrate;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        _cooldown -= Time.deltaTime;
+
+        if (_cooldown <= 0.0f)
+        {
+            SpawnEnergy();
+            ResetTimer();
+        }
+	}
+
+    private bool SpawnEnergy()
+    {
+        GameObject[] spawns = GameObject.FindGameObjectsWithTag("Spawn");
+
+        System.Random rand = new System.Random();
+        int spawnSelect = rand.Next(spawns.Length - 1);
+        Debug.Log(spawnSelect);
+
+        return spawns[spawnSelect].GetComponent<SpawnActivate>().Activate();
+    }
+
+    public void ResetTimer()
+    {
+        _cooldown = _spawnrate;
+    }
+}
---
+++ WaveHockey/Assets/Scripts/SpawnActivate.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnActivate : MonoBehaviour {
+
+    public GameObject _energy;
+    public GameObject _gameController;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    public bool Activate()
+    {
+        if (transform.childCount <= 0)
+        {
+            var energyClone = Instantiate(_energy, transform.position, Quaternion.identity);
+            energyClone.transform.parent = transform;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
